286bb2d17b96e60be2aab2fd736ea11e11d921a8
Kent <cskjsw@gmail.com>
2023-06-26 01:42:03 +0800

    Login validation. 1. Accepts any valid email + password of "Testpassw0rd!" will lead to main screen. 2. Show 3 incorrect attempts, displays an error and disable the login button for 1 minute. 3. Forgot password just opens a popupwindow that loads google.com.
    Added better UX, shows countdown before loggin in.
    shows an error every attempt.

--- Enstack/src/screens/LoginScreen.js
+++ Enstack/src/screens/LoginScreen.js
@@ -1,19 +1,139 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Linking,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
 
 const LoginScreen = ({ navigation }) => {
   const auth = useAuth();
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [attempts, setAttempts] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [invalidCredentialsMessage, setInvalidCredentialMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [disableLogin, setDisableLogin] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+
+  useEffect(() => {
+    let interval;
+
+    if (attempts >= 3) {
+      setDisableLogin(true);
+      setCountdown(60);
+
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [attempts]);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      setDisableLogin(false);
+      setAttempts(0);
+      setInvalidCredentialMessage("");
+      setErrorMessage("");
+    }
+  }, [countdown]);
+
+  const validateEmail = () => {
+    // Email validation regex pattern
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const handleLogin = () => {
+    setIsLoading(true);
+
+    // Simulating API call for authentication
+    setTimeout(() => {
+      if (validateEmail() && password === "Testpassw0rd!") {
+        login();
+      } else {
+        setInvalidCredentialMessage("Invalid email or password.");
+        // Incorrect login attempt
+        setAttempts(attempts + 1);
+        setIsLoading(false);
+
+        if (attempts >= 2) {
+          setErrorMessage("Too many incorrect attempts.");
+          setInvalidCredentialMessage("");
+        }
+      }
+    }, 2000); // Simulated delay for API call
+  };
+
+  const handleForgotPassword = () => {
+    // Open a popup window or navigate to the password recovery page
+    // In this example, we will open google.com in a new tab/window
+    Linking.openURL("https://google.com");
+  };
 
   return (
-    <View>
-      <Text>Hello Login</Text>
-      <Button title="Login" onPress={auth.login} />
-      {/* <Button title="Signup" onPress={() => navigation.navigate("Signup")} /> */}
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Email:</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1 }}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        keyboardType="email-address"
+      />
+      <Text>Password:</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1 }}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+      <Button
+        title="Login"
+        onPress={handleLogin}
+        disabled={disableLogin || isLoading}
+      />
+      {attempts > 0 ? (
+        <Text style={{ color: "red" }}>{invalidCredentialsMessage}</Text>
+      ) : (
+        <></>
+      )}
+
+      {disableLogin && (
+        <View>
+          {countdown > 0 ? (
+            <View style={{ alignItems: "center" }}>
+              <Text style={{ color: "red" }}>{errorMessage}</Text>
+              <Text>{`Try again in ${countdown} seconds`}</Text>
+            </View>
+          ) : (
+            <></>
+          )}
+        </View>
+      )}
+      <TouchableOpacity onPress={handleForgotPassword}>
+        <Text>Forgot password?</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  loginForm: {
+    alignItems: "center",
+  },
+  headerLogin: {
+    alignItems: "center",
+  },
+});
 
 export default LoginScreen;
