6bdd521390622a2d0ca2332eb04e934d673f64cb
Kent <cskjsw@gmail.com>
2023-06-26 03:18:40 +0800

    Add mock items Add FlatList in HomeScreen Add design Pass Product Details by props

---
+++ Enstack/src/data/items.js
@@ -0,0 +1,113 @@
+import mainChairImage from "../../assets/chair.jpeg";
+import chair2 from "../../assets/chair2.jpeg";
+import chair3 from "../../assets/chair3.jpeg";
+
+import mainBedImage from "../../assets/bed.jpeg";
+import bed2 from "../../assets/bed2.jpeg";
+import bed3 from "../../assets/bed3.jpeg";
+
+import mainLampImage from "../../assets/lamp.jpeg";
+import lamp2 from "../../assets/lamp2.jpeg";
+import lamp3 from "../../assets/lamp3.jpeg";
+
+import mainTableImage from "../../assets/table.jpeg";
+import table2 from "../../assets/table2.jpeg";
+import table3 from "../../assets/table3.jpeg";
+
+import mainArmChairImage from "../../assets/armchair.jpeg";
+import armchair2 from "../../assets/armchair2.jpeg";
+import armchair3 from "../../assets/armchair3.jpeg";
+
+export const productItemsData = [
+  {
+    type: "chair",
+    name: "Black Simple Lamp",
+    price: 12.0,
+    main_image: mainLampImage,
+    product_images: [lamp2, lamp3],
+    priority: 1,
+    product_id: "00000001",
+    description: "Lorem Ipsum...",
+    rating: 4.75,
+    review_count: 50,
+    stock: 5,
+  },
+  {
+    type: "table",
+    name: "Black Simple Table",
+    price: 12.61,
+    main_image: mainChairImage,
+    product_images: [chair2, chair3],
+    priority: 1,
+    product_id: "00000002",
+    description: "Lorem Ipsum...",
+    rating: 4.1,
+    review_count: 10,
+    stock: 50,
+  },
+  {
+    type: "armchair",
+    name: "Black Simple Armchair",
+    price: 1.61,
+    main_image: mainArmChairImage,
+    product_images: [armchair2, armchair3],
+    priority: 0,
+    product_id: "00000003",
+    description: "Lorem Ipsum...",
+    rating: 1.1,
+    review_count: 100,
+    stock: 0,
+  },
+  {
+    type: "bed",
+    name: "Black Simple Bed",
+    price: 2.61,
+    main_image: mainBedImage,
+    product_images: [bed2, bed3],
+    priority: 10,
+    product_id: "00000004",
+    description: "Lorem Ipsum...",
+    rating: 0,
+    review_count: 0,
+    stock: 2,
+  },
+  {
+    type: "chair",
+    name: "White Simple Chair",
+    price: 99.0,
+    main_image: mainChairImage,
+    product_images: [chair2, chair3],
+    priority: 12,
+    product_id: "00000005",
+    description: "Lorem Ipsum...",
+    rating: 0,
+    review_count: 0,
+    stock: 0,
+  },
+  {
+    type: "table",
+    name: "White Simple Table",
+    price: 80.0,
+    main_image: mainTableImage,
+    product_images: [table2, table3],
+    priority: 0,
+    product_id: "00000006",
+    description: "Lorem Ipsum...",
+    rating: 5.0,
+    review_count: 1000,
+    stock: 2,
+  },
+  {
+    type: "armchair",
+    name: "White Simple Armchair",
+    price: 1000000.5,
+    main_image: mainArmChairImage,
+    product_images: [armchair2, armchair3],
+    priority: 0,
+    product_id: "00000007",
+    description: "Lorem Ipsum...",
+    rating: 3.0,
+    review_count: 1,
+    stock: 1,
+  },
+];
--- Enstack/src/screens/HomeScreen.js
+++ Enstack/src/screens/HomeScreen.js
@@ -1,20 +1,97 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { productItemsData } from "../data/items";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  return (
+  const renderItem = ({ item }) => (
     <View>
-      <Text>Hello Home</Text>
-      <Button
-        title="Go to Product"
-        onPress={() => navigation.navigate("Product")}
+      <TouchableOpacity
+        style={styles.itemContainer}
+        onPress={() => navigation.navigate("Product", { product: item })}
+      >
+        <Image
+          source={item.main_image}
+          style={styles.itemImage}
+          resizeMode="cover"
+        />
+        <TouchableOpacity style={styles.addToCartButton}>
+          <Text style={styles.addToCartButtonText}>C</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+      <View>
+        <View styles={styles.productDetailsView}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text style={styles.itemPrice}>${item.price}</Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={productItemsData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.product_id}
+        numColumns={2}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  itemContainer: {
+    marginTop: 10,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  itemImage: {
+    width: 170,
+    height: 260,
+    margin: 5,
+    borderRadius: 10,
+  },
+  productDetailsView: {
+    flexDirection: "row",
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+    maxWidth: 150,
+  },
+  itemPrice: {
+    fontSize: 14,
+    color: "#888",
+    marginBottom: 8,
+  },
+  addToCartButton: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginRight: 10,
+    marginBottom: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    zIndex: 1,
+  },
+  addToCartButtonText: {
+    color: "#fff",
+  },
+});
 
 export default HomeScreen;
